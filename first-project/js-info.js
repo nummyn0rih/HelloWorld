603478b85abceabba9fd656776f4f8f3498eb797
Ivan Malshchukov <43786840+nummyn0rih@users.noreply.github.com>
2025-05-23 14:40:50 +0300

    Модуль 1. База JavaScript 1. Hello World

---
+++ first-project/js-info.js
@@ -0,0 +1,5 @@
+alert('Первая реализация JavaScript была создана Бренданом Эйхом в компании Netscape');
+alert('JavaScript был разработан за 10 дней');
+alert('Первая версия языка JavaScript была разработана в 1995 году');
+alert('JavaScript — это не Java');
+alert('ECMAScript');
