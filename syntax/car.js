a54486cd5a8cc1c8cc85c04b44b9f1a611bcce7d
Ivan Malshchukov <43786840+nummyn0rih@users.noreply.github.com>
2025-05-26 16:22:17 +0300

    Модуль 1. База JavaScript 2. Синтаксис

---
+++ syntax/car.js
@@ -0,0 +1,21 @@
+const carName = 'Nissan';
+let maxCarSpeed = 190;
+let carOwner = 'Иван';
+
+alert(carName);
+alert(maxCarSpeed);
+alert(carOwner);
+
+maxCarSpeed = 185;
+console.log('У машины изменилась скорость', maxCarSpeed);
+maxCarSpeed = 180;
+console.log('У машины изменилась скорость', maxCarSpeed);
+maxCarSpeed = 175;
+console.log('У машины изменилась скорость', maxCarSpeed);
+
+carOwner = 'Федор';
+console.log('У машины изменилась скорость', carOwner);
+carOwner = 'Анна';
+console.log('У машины изменилась скорость', carOwner);
+carOwner = 'Ульяна';
+console.log('У машины изменилась скорость', carOwner);
---
+++ syntax/index.js
@@ -0,0 +1,18 @@
+// JavaScript-код с созданием переменных
+
+/*
+Автор:
+Иван Мальщуков
+*/
+
+const myName = 'Иван';
+const mySurname = 'Мальщуков';
+const myFavoriteDrink = 'Молоко';
+const myFavoriteAnimal = 'Собака';
+const myFavoriteProgrammingLanguage = 'JavaScript';
+
+console.log(myName);
+console.log(mySurname);
+console.log(myFavoriteDrink);
+console.log(myFavoriteAnimal);
+console.log(myFavoriteProgrammingLanguage);
