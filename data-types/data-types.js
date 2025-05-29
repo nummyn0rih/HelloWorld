2e18194b2d2e7765feefad50522ceb56307832b8
Ivan Malshchukov <43786840+nummyn0rih@users.noreply.github.com>
2025-05-29 09:28:26 +0300

    Модуль 1. База JavaScript 3. Типы данных

---
+++ data-types/data-types.js
@@ -0,0 +1,26 @@
+const number = 22;
+console.log('number', typeof number);
+ 
+const string = 'date';
+console.log('string', typeof string);
+ 
+const bool = true;
+console.log('bool', typeof bool);
+ 
+const nothing = null;
+console.log('nothing', typeof nothing);
+ 
+let boom;
+console.log('boom', typeof boom);
+ 
+const object = {
+    name: 'Ivan',
+    lang: 'JS'
+};
+console.log('object', typeof object);
+ 
+const bigint = 22n;
+console.log('bigint', typeof bigint);
+ 
+const symbol = Symbol('next');
+console.log('symbol', typeof symbol);
---
+++ data-types/index.js
@@ -0,0 +1,11 @@
+let number = 22;
+let string  = 'two';
+let bool = true;
+
+number = 'twenty one';
+string = 2;
+bool = null;
+
+alert(number);
+alert(string);
+alert(bool);
