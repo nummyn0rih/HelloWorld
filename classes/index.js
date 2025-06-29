9794adf24032a32cabec4f857a76a05f9b095ab2
Ivan Malshchukov <43786840+nummyn0rih@users.noreply.github.com>
2025-06-29 23:57:00 +0300

    Update index.js

--- classes/index.js
+++ classes/index.js
@@ -57,9 +57,9 @@ const person1 = new Person('Максим', 24);
 const person2 = new Person('Светлана', 36);
 const person3 = new Person('Ирина', 23);
 
-// person1.compareAge(person2); // Максим младше, чем Светлана 
-// person2.compareAge(person3); // Светлана старше, чем Ирина 
-// person3.compareAge(person1); // Ирина младше, чем Максим
+person1.compareAge(person2); // Максим младше, чем Светлана 
+person2.compareAge(person3); // Светлана старше, чем Ирина 
+person3.compareAge(person1); // Ирина младше, чем Максим
 
 // --------------------------------------------------
 // Задание #3
